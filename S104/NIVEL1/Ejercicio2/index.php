428bc4225a2fa48426a6923191351c9fa019969b
Fabian <fabiane.cpp.1005@gmail.com>
2025-07-02 11:35:47 +0200

    se modifico en index.php, creando 2 objetos de prueba (un triangulo y un rectangulo, imprimiendo su area)

--- S104/NIVEL1/Ejercicio2/index.php
+++ S104/NIVEL1/Ejercicio2/index.php
@@ -1,2 +1,12 @@
 <?php
 
+include "shape.php";
+include "triangulo.php";
+include "rectangulo.php";
+
+$triangulo1 = new Triangulo(5, 3);
+$rectangulo1 = new Rectangulo(6, 5);
+
+echo $triangulo1->area();
+echo "\n";
+echo $rectangulo1->area();
