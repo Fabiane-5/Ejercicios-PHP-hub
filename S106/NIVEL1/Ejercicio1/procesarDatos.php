33f2b939d54931a748c7a535f3915046e7d7d876
Fabian <fabiane.cpp.1005@gmail.com>
2025-07-08 13:39:03 +0200

    se creo un formulario y se mando los atos por POST a un archivo procesarDatos.php, se mostro con la misma variable superglobal, se guardo en una variable session, para mostrar en otro archivo

--- S106/NIVEL1/Ejercicio1/procesarDatos.php
+++ S106/NIVEL1/Ejercicio1/procesarDatos.php
@@ -1,3 +1,14 @@
 <?php
 session_start();
-echo $_POST ['nombres'];
+echo "<h1>Datos Guardados</h1>";
+echo "Nombre: " . $_POST['nombre'] . "\n";
+echo "Apellido: " . $_POST['apellido'] . "\n";
+echo "Email: " . $_POST['email'] . "\n";
+echo "Telefono: " . $_POST['telefono'] . "\n";
+echo "Fecha de nacimiento: " . $_POST['date'] . "\n";
+
+$_SESSION['nombre'] = $_POST['nombre'];
+$_SESSION['apellido'] = $_POST['apellido'];
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['telefono'] = $_POST['telefono'];
+$_SESSION['date'] = $_POST['date'];
